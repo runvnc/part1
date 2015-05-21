120eeb082a056ef8aa0443f0db9f55400707de57
Jason Livesay <ithkuil@gmail.com>
2015-05-21 05:52:34 +0000

    client js

---
+++ dist/public/client.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var app = {
+  list: function list() {},
+
+  collectFields: function collectFields() {},
+
+  saveNew: function saveNew() {},
+
+  saveEdit: function saveEdit() {}
+
+};
+
+$(function () {
+  alert('test');
+  // list
+
+  // edit click
+
+  // save new / edit
+
+  // delete
+});
---
+++ src/client.js
@@ -0,0 +1,31 @@
+var app = {
+  list() {
+
+  },
+
+  collectFields() {
+
+  },
+
+  saveNew() {
+
+  },
+
+  saveEdit() {
+
+
+  }
+
+}
+
+$( () => {
+  alert('test');
+  // list
+
+  // edit click
+
+  // save new / edit
+
+  // delete
+  }
+)
